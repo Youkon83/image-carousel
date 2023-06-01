0b82173bdcc0c3f3cadb71926e979b422db04d8d
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2023-06-01 14:19:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let i = 0
+
+let interval = setInterval(run, 2000)
+
+function run(){
+    i++
+
+    changeImage()
+}
+
+function changeImage(){
+    if(i > img.length - 1){
+        i = 0
+    }
+    else if(i < 0){
+        i = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-i * 500}px)`
+}
+
+function resetInterval(){
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+leftBtn.addEventListener('click', () =>{
+    i--
+    changeImage()
+    resetInterval()
+})
+rightBtn.addEventListener('click', () =>{
+    i++
+    changeImage()
+    resetInterval()
+})
